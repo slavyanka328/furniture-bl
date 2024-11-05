36755cb02d538b9b279b0c79a69196e248f18f22
slavyanka328 <sornikovsvatoslav28@gmail.com>
2024-11-05 18:10:27 +0500

    Add files via upload

---
+++ Furniture/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furniture
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Category = new Category(); // По умолчанию создаем пустую категорию
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Цена: {Price}");
+            Console.WriteLine("-------------------------");
+        }
+    }
+}
